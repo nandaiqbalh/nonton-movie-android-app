5067ad273ff5c831a6165c9258c3100ead51d7e9
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-28 22:03:06 +0700

    FE: Home Screen

---
+++ app/src/main/java/com/nandaiqbalh/nontonmoviekotlin/authentication/signup/SignUpPhotoScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.nandaiqbalh.nontonmoviekotlin.authentication.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nandaiqbalh.nontonmoviekotlin.R
+
+class SignUpPhotoScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_photo)
+    }
+}
---
+++ app/src/main/java/com/nandaiqbalh/nontonmoviekotlin/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.nandaiqbalh.nontonmoviekotlin.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nandaiqbalh.nontonmoviekotlin.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
