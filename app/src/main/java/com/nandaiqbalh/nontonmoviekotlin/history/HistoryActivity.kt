b738d9ecff6b55f2f7a3761e4cd0a96b9b0fec7a
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-29 09:27:38 +0700

    FE: History

---
+++ app/src/main/java/com/nandaiqbalh/nontonmoviekotlin/history/HistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.nandaiqbalh.nontonmoviekotlin.history
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nandaiqbalh.nontonmoviekotlin.R
+
+class HistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
