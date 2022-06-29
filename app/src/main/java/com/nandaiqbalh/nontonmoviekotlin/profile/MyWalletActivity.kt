c7229cc7fd04fe08630b72fc1b9e664ce28d7e96
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-29 09:24:57 +0700

    FE: My Wallet

---
+++ app/src/main/java/com/nandaiqbalh/nontonmoviekotlin/profile/MyWalletActivity.kt
@@ -0,0 +1,12 @@
+package com.nandaiqbalh.nontonmoviekotlin.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nandaiqbalh.nontonmoviekotlin.R
+
+class MyWalletActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_wallet2)
+    }
+}
