10e477d1cea3c9dc74e97dd6d610af35e63e0f8d
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-29 09:09:58 +0700

    Checkout

---
+++ app/src/main/java/com/nandaiqbalh/nontonmoviekotlin/checkout/CheckoutActivity.kt
@@ -0,0 +1,12 @@
+package com.nandaiqbalh.nontonmoviekotlin.checkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nandaiqbalh.nontonmoviekotlin.R
+
+class CheckoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout)
+    }
+}
