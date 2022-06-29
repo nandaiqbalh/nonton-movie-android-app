2f90b5137c4bdad97ed7476f8250039e6c3e9a00
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-29 09:04:30 +0700

    FE: Select Seat

---
+++ app/src/main/java/com/nandaiqbalh/nontonmoviekotlin/home/SelectSeatActivity.kt
@@ -0,0 +1,12 @@
+package com.nandaiqbalh.nontonmoviekotlin.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nandaiqbalh.nontonmoviekotlin.R
+
+class SelectSeatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_seat)
+    }
+}
