42431b0551c870e697176a91e71b181052aa07b0
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-30 21:11:39 +0700

    Now Playing Movies

--- app/src/main/java/com/nandaiqbalh/nontonmoviekotlin/home/fragment/DashboardFragment.kt
+++ app/src/main/java/com/nandaiqbalh/nontonmoviekotlin/home/fragment/DashboardFragment.kt
@@ -1,11 +1,27 @@
 package com.nandaiqbalh.nontonmoviekotlin.home.fragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.database.*
 import com.nandaiqbalh.nontonmoviekotlin.R
+import com.nandaiqbalh.nontonmoviekotlin.adapter.NowPlayingAdapter
+import com.nandaiqbalh.nontonmoviekotlin.model.Film
+import com.nandaiqbalh.nontonmoviekotlin.utils.SharedPrefs
+import java.text.NumberFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,12 +46,32 @@ class DashboardFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+
+    // variables
+
+    private lateinit var sharedPrefs: SharedPrefs
+    private lateinit var mDatabase: DatabaseReference
+
+    private lateinit var tvName: TextView
+    private lateinit var tvSaldo:TextView
+    private lateinit var ivProfile: ImageView
+
+    private lateinit var rvNowPlaying: RecyclerView
+    private lateinit var rvComingSoon: RecyclerView
+
+    private var dataList = ArrayList<Film>()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        var view = inflater.inflate(R.layout.fragment_dashboard, container, false)
+
+        // init
+        init(view)
+
+        // atur data
+        aturData()
+
+        return view
     }
 
     companion object {
@@ -56,5 +92,77 @@ class DashboardFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    private fun init(view: View){
+
+        sharedPrefs = SharedPrefs(requireActivity().applicationContext)
+        mDatabase = FirebaseDatabase.getInstance().getReference("Film")
+
+        tvName = view.findViewById(R.id.tv_name)
+        tvSaldo = view.findViewById(R.id.tv_saldo)
+        ivProfile = view.findViewById(R.id.iv_profile)
+
+        rvNowPlaying = view.findViewById(R.id.rv_now_playing)
+        rvComingSoon = view.findViewById(R.id.rv_coming_soon)
+
+    }
+
+    private fun aturData(){
+        tvName.setText(sharedPrefs.getValue("name"))
+        Log.d("Nama", "Nama: " + sharedPrefs.getValue("name"))
+        Log.d("Saldo", "Saldo: " + sharedPrefs.getValue("saldo"))
+
+
+        if (sharedPrefs.getValue("saldo") != ""){
+
+            currencyFormatter(sharedPrefs.getValue("saldo")!!.toDouble(), tvSaldo)
+        }
+
+        Glide.with(this)
+            .load(sharedPrefs.getValue("url"))
+            .apply(RequestOptions.circleCropTransform())
+            .into(ivProfile)
+
+        rvNowPlaying.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        rvComingSoon.layoutManager = LinearLayoutManager(context) // defaultnya udah vertical, jadi gaperlu atur2
+
+        // getData
+        getData()
+    }
+
+    private fun getData() {
+        mDatabase.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                // clear datalist agar tidak ada data yang duplikasi
+                dataList.clear()
+
+                for (getDataSnapshot in snapshot.children){
+
+                    // tampung jika ada valuenya
+                    var film = getDataSnapshot.getValue(Film::class.java)
+                    dataList.add(film!!)
+
+                    rvNowPlaying.adapter  = NowPlayingAdapter(dataList){
+
+                        // jika dipencet akan ke halaman detail
+
+                    }
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, "Error: ${error.message}", Toast.LENGTH_LONG).show();
+            }
+
+        })
+    }
+
+    private fun currencyFormatter(harga: Double, textView: TextView){
+        val localID = Locale("in", "ID")
+        val format = NumberFormat.getCurrencyInstance(localID)
+        textView.setText(format.format(harga))
     }
 }
---
+++ app/src/main/java/com/nandaiqbalh/nontonmoviekotlin/model/Film.kt
@@ -0,0 +1,15 @@
+package com.nandaiqbalh.nontonmoviekotlin.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class Film (
+
+    var desc: String? = "",
+    var director: String? = "",
+    var genre: String? = "",
+    var judul: String? = "",
+    var poster: String? = "",
+    var rating: String? = "",
+) : Parcelable
