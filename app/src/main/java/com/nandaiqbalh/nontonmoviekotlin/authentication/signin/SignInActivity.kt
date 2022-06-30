eb0a0cefab4e34a0d8b58fcb7a35af076877c71d
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-30 10:59:25 +0700

    Set up Firebase as DatabasEe

--- app/src/main/java/com/nandaiqbalh/nontonmoviekotlin/authentication/signin/SignInActivity.kt
+++ app/src/main/java/com/nandaiqbalh/nontonmoviekotlin/authentication/signin/SignInActivity.kt
@@ -1,12 +1,49 @@
 package com.nandaiqbalh.nontonmoviekotlin.authentication.signin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.nandaiqbalh.nontonmoviekotlin.R
 
 class SignInActivity : AppCompatActivity() {
+
+    lateinit var btnSignIn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        // full screen
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        // init
+        init()
+
+        // button pressed
+        mainButton()
+
+    }
+
+    private fun init() {
+
+        btnSignIn = findViewById(R.id.btn_sign_in)
+    }
+
+    private fun mainButton() {
+
+        btnSignIn.setOnClickListener {
+
+            // Write a message to the database
+            val database = Firebase.database
+            val myRef = database.getReference("message")
+
+            myRef.setValue("Hello, World!")
+        }
     }
 }
