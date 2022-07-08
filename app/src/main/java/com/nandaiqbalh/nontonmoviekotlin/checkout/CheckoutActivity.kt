ae572daad47d733f9c3ab43b5133b03ad2b2f761
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-07-08 09:34:38 +0700

    Add Notification

--- app/src/main/java/com/nandaiqbalh/nontonmoviekotlin/checkout/CheckoutActivity.kt
+++ app/src/main/java/com/nandaiqbalh/nontonmoviekotlin/checkout/CheckoutActivity.kt
@@ -1,11 +1,18 @@
 package com.nandaiqbalh.nontonmoviekotlin.checkout
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import androidx.core.app.NotificationCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.nandaiqbalh.nontonmoviekotlin.R
@@ -91,7 +98,50 @@ class CheckoutActivity : AppCompatActivity() {
         btnCheckout.setOnClickListener {
             var intent = Intent(this@CheckoutActivity, CheckoutSuccessActivity::class.java)
             startActivity(intent)
+
+            // show notification
+            showNotif()
         }
 
+    }
+
+    private fun showNotif(){
+
+        val NOTIFICATION_CHANNEL_ID = "nonton_movie" // jika kita memiliki banyak notifikasi, ID ini nanti bisa dijadikan penanda
+        val context = this.applicationContext
+        var notificationManager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
+            val channelName = "NontonMovie Channel"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+
+            val mChannel = NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, importance)
+            notificationManager.createNotificationChannel(mChannel)
+        }
+
+        // on click notification
+        val mIntent = Intent(this, CheckoutSuccessActivity::class.java)
+        val bundle = Bundle()
+        bundle.putString("id", "id_film")
+        mIntent.putExtras(bundle)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0 , mIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+        builder.setContentIntent(pendingIntent)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setLargeIcon(
+                BitmapFactory.decodeResource(this.resources, R.mipmap.ic_launcher)
+            )
+            .setTicker("notif nontonmovie starting")
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+            .setLights(Color.RED, 3000, 3000)
+            .setDefaults(Notification.DEFAULT_SOUND)
+            .setContentTitle("Payment Successful!")
+            .setContentText("NontonMovie")
+
+        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(115, builder.build())
     }
 }
